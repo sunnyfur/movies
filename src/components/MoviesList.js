bfb7bbba1bc97f1b75c0fac4339b242a5a962f9a
Polina <sunnyfur9@gmail.com>
2023-02-09 23:29:03 +0500

    feat: add loader to movies

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,18 +1,32 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { getListMovies } from "../api/moviesApi";
+import Loader from "./Loader";
 const MoviesList = () => {
   const dispatch = useDispatch();
-  const { movies } = useSelector((state) => state.movies);
+  const { movies, isLoadingMovies } = useSelector((state) => state.movies);
   useEffect(() => {
     dispatch(getListMovies());
   }, []);
+  const navigate = useNavigate();
   const handleClick = () => {};
-
+  const handleMovieClick = (event) => {
+    // console.log(event.currentTarget.dataset.id);
+    navigate(`/films/${event.currentTarget.dataset.id}`);
+  };
+  if (isLoadingMovies) return <Loader />;
   return (
     <>
       {movies.map((movie) => (
-        <p key={movie.filmId}>{movie.nameRu}</p>
+        <div
+          key={movie.filmId}
+          data-id={movie.filmId}
+          onClick={handleMovieClick}
+        >
+          <p>{movie.filmId}</p>
+          <p>{movie.nameRu}</p>
+        </div>
       ))}
       <button onClick={handleClick}>Next</button>
     </>
